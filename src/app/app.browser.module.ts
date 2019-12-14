a7ded78c12bfe70a654dbcf6d4087bdd970aac7b
Warren Dugan <warrendugan@gmail.com>
2019-12-13 23:52:23 -0800

    PWA and CI Action.

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppModule } from './app.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -12,7 +14,8 @@ import { AppModule } from './app.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     AppModule,
-    BrowserTransferStateModule
+    BrowserTransferStateModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
