20f2bb40d0fd453c677ee078bfed39efba65f33d
Warren Dugan <warrendugan@gmail.com>
2019-12-15 18:57:41 -0800

    Removing rendertron and hardcoding open graph meta tags.

--- server.ts
+++ server.ts
@@ -16,16 +16,11 @@
  */
 
 import 'zone.js/dist/zone-node';
-import * as rendertron from 'rendertron-middleware';
 import * as express from 'express';
 import { join } from 'path';
 
 // Express server
 export const app = express();
-
-app.use(rendertron.makeMiddleware({
-  proxyUrl: 'https://render-tron.appspot.com/',
-}));
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  private titleValue = 'Goldmine Festival';
-  private description = 'Country music has found a new home at Big Bear Mountain. Goldmine Festival will debut in Fall 2021 with big country music stars.';
-  constructor(private meta: Meta, private title: Title) { }
-
-  ngOnInit() {
-    this.title.setTitle(this.titleValue);
-    this.meta.addTags([
-      { name: 'description', content: this.description },
-      { name: 'keywords', content: 'Country, Country Music, Festival, Big Bear, Big Bear Mountain, Goldmine, Goldmine Country Music Festival, Goldmine Festival' },
-      { name: 'robots', content: 'index, follow' },
-      { name: 'author', content: this.titleValue },
-      { name: 'date', content: '12-15-2019', scheme: 'MM-DD-YYYY' },
-      { name: 'og:image', content: 'https://goldminefestival.com/assets/mountain-bg.png' },
-      { name: 'og:image:alt', content: this.titleValue },
-      { name: 'og:image:width', content: '1200' },
-      { name: 'og:image:height', content: '630' },
-      { charset: 'UTF-8' }
-    ]);
-    const canonical: HTMLLinkElement = document.createElement('link');
-    canonical.setAttribute('rel', 'canonical');
-    canonical.setAttribute('href', document.URL);
-    document.head.appendChild(canonical);
-  }
-}
+export class AppComponent { }
